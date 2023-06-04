8f8645315178a7338dcdbdcc616794d40466d89c
Jovan Stojimirovic <jocas03@hotmail.com>
2023-06-04 17:35:18 +0200

    created employees dto class

---
+++ LibraryManagement.Webapp/Dto/EmployeeDto.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagement.Application.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Webapp.Dto
+{
+    public record EmployeeDto(
+        Guid Guid,
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Der Vorname muss zwischen 2 und 30 Zeichen lang sein.")]
+        string Firstname,
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Der Vorname muss zwischen 2 und 30 Zeichen lang sein.")]
+        string Lastname,
+        Guid RoleGuid
+        );
+}
---
+++ LibraryManagement.Webapp/Dto/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using LibraryManagement.Application.Models;
+
+namespace LibraryManagement.Webapp.Dto
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AuthorDto, Author>();
+            CreateMap<Author, AuthorDto>();
+
+            CreateMap<EmployeeDto, Employee>();
+            CreateMap<Employee, EmployeeDto>();
+        }
+    }
+}
