fc1fadfd143a59dc0b1b79af97ea381565699e9f
Jovan Stojimirovic <jocas03@hotmail.com>
2023-06-04 17:33:10 +0200

    created author dto class

---
+++ LibraryManagement.Webapp/Dto/AuthorDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Webapp.Dto
+{
+    public record AuthorDto(
+        Guid Guid,
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Der Vorname muss zwischen 2 und 30 Zeichen lang sein.")]
+        string Firstname,
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Der Vorname muss zwischen 2 und 30 Zeichen lang sein.")]
+        string Lastname,
+        DateTime BirthDate,
+        [StringLength(70, MinimumLength = 2, ErrorMessage = "Die Nationalität muss zwischen 2 und 70 Zeichen lang sein.")]
+        string Nationality
+        );
+}
