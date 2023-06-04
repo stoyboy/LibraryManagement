a523bafba69ef5e0d4620fc9cbd4aaee76884bf9
Jovan Stojimirovic <jocas03@hotmail.com>
2023-06-04 20:26:13 +0200

    bugfix bookdto validation

--- LibraryManagement.Webapp/Dto/BookDto.cs
+++ LibraryManagement.Webapp/Dto/BookDto.cs
@@ -4,7 +4,7 @@ namespace LibraryManagement.Webapp.Dto
 {
     public record BookDto(
         Guid Guid,
-        [StringLength(30, MinimumLength = 2, ErrorMessage = "Der Verlag muss zwischen 2 und 30 Zeichen lang sein.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Der Verlag muss zwischen 2 und 30 Zeichen lang sein.")]
         string Publisher,
         int Year,
         [Range(0, 10, ErrorMessage = "Bewertung muss zwischen 0 und 10 Punkten sein.")]
