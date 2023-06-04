3852f087d3923320a98255e6253c96aa101e879c
Jovan Stojimirovic <jocas03@hotmail.com>
2023-06-04 17:32:09 +0200

    created database tests

---
+++ LibraryManagement.Test/DatabaseTest.cs
@@ -0,0 +1,37 @@
+﻿using LibraryManagement.Application.Infrastructure;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Test
+{
+    public class DatabaseTest : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        protected readonly LibraryContext _db;
+
+        public DatabaseTest()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+            var opt = new DbContextOptionsBuilder()
+                .UseSqlite(_connection)  // Keep connection open (only needed with SQLite in memory db)
+                .UseLazyLoadingProxies()
+                .LogTo(message => Debug.WriteLine(message), Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .Options;
+
+            _db = new LibraryContext(opt);
+        }
+        public void Dispose()
+        {
+            _db.Dispose();
+            _connection.Dispose();
+        }
+    }
+}
---
+++ LibraryManagement.Test/LibraryContextTest.cs
@@ -0,0 +1,39 @@
+﻿using LibraryManagement.Application.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Test
+{
+    public class LibraryContextTest
+    {
+        private LibraryContext GetDatabase(bool deleteDb = false)
+        {
+            var db = new LibraryContext(new DbContextOptionsBuilder().UseSqlite("Data Source=OSHypervisor.db").UseLazyLoadingProxies().Options);
+
+            if (deleteDb)
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+            }
+
+            return db;
+        }
+
+        [Fact]
+        public void CreateDatabaseSuccessTest()
+        {
+            using var db = GetDatabase(deleteDb: true);
+        }
+
+        [Fact]
+        public void SeedDatabaseTest()
+        {
+            using var db = GetDatabase(deleteDb: true);
+            db.Seed();
+        }
+    }
+}
