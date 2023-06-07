1f67549697d1dc76f4ef8919ecd2a343a3bd7df5
Jovan Stojimirovic <jocas03@hotmail.com>
2023-06-07 03:44:10 +0200

    added additional validation

--- LibraryManagement.Webapp/Dto/AuthorDto.cs
+++ LibraryManagement.Webapp/Dto/AuthorDto.cs
@@ -2,12 +2,27 @@
 
 namespace LibraryManagement.Webapp.Dto
 {
+    class ValidBirthDate : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var author = validationContext.ObjectInstance as AuthorDto;
+            if (author is null) { return null; }
+
+            if (author.BirthDate > DateTime.Now.AddYears(-18))
+                return new ValidationResult("Der Autor muss mindestens 18 Jahre alt sein.");
+
+            return ValidationResult.Success;
+        }
+    }
+
     public record AuthorDto(
         Guid Guid,
         [StringLength(30, MinimumLength = 2, ErrorMessage = "Der Vorname muss zwischen 2 und 30 Zeichen lang sein.")]
         string Firstname,
         [StringLength(30, MinimumLength = 2, ErrorMessage = "Der Vorname muss zwischen 2 und 30 Zeichen lang sein.")]
         string Lastname,
+        [ValidBirthDate]
         DateTime BirthDate,
         [StringLength(70, MinimumLength = 2, ErrorMessage = "Die Nationalität muss zwischen 2 und 70 Zeichen lang sein.")]
         string Nationality
--- LibraryManagement.Webapp/Dto/BookDto.cs
+++ LibraryManagement.Webapp/Dto/BookDto.cs
@@ -2,11 +2,25 @@
 
 namespace LibraryManagement.Webapp.Dto
 {
+    class ValidYear : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var book = validationContext.ObjectInstance as BookDto;
+            if (book is null) { return null; }
+
+            if (book.Year > DateTime.Now.Year)
+                return new ValidationResult("Das Datum darf nicht in der Zukunft liegen.");
+
+            return ValidationResult.Success;
+        }
+    }
     public record BookDto(
         Guid Guid,
         [StringLength(100, MinimumLength = 2, ErrorMessage = "Der Verlag muss zwischen 2 und 30 Zeichen lang sein.")]
         string? Title,
         string Publisher,
+        [ValidYear]
         int Year,
         [Range(0, 10, ErrorMessage = "Bewertung muss zwischen 0 und 10 Punkten sein.")]
         int Rating
